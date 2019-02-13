5fa91132dc19d00b34b298f5f9eac90b6e3e7314
Mike Barnett <mbarnett@microsoft.com>
2019-02-13 10:35:21 -0800

    Beginning of video annotation capability. For now, just a button...

---
+++ js/annotate.js
@@ -0,0 +1,44 @@
+﻿(function (g) {
+    "use strict";
+
+    let isAnnotating = false;
+
+
+    window.onload = function () {
+        initialize();
+    };
+
+    function initialize() {
+        try {
+            document
+                .getElementById("annotate_btn")
+                .addEventListener("click", onAnnotateButtonPressed);
+        } catch (e) {
+            showMessage("Initilization error: " + e.message || e, true);
+        }
+    }
+
+    function onAnnotateButtonPressed() {
+        if (isAnnotating) {
+            stopAnnotating();
+        } else {
+            startAnnotating();
+        }
+    }
+
+    function stopAnnotating() {
+        let e = document.getElementById("annotate_btn");
+        let v = e.value;
+        e.value = "Start Annotate";
+        isAnnotating = false;
+    }
+
+    function startAnnotating() {
+        let e = document.getElementById("annotate_btn");
+        let v = e.value;
+        e.value = "Stop Annotate";
+        isAnnotating = true;
+    }
+
+
+})(typeof window === "object" ? window : global);
