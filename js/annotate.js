deba41919f0fab1119af89070e2e9a51bf0ef09b
Mike Barnett <mbarnett@microsoft.com>
2019-02-26 14:04:25 -0800

    Since the code in annotate.js doesn't use the parameter, remove it from being wrapped in a function.

--- js/annotate.js
+++ js/annotate.js
@@ -1,44 +1,98 @@
-﻿(function (g) {
-    "use strict";
+﻿
+// Modifying the RTC video using information from this site:
+// https://deepstreamhub.com/tutorials/protocols/webrtc-video-manipulation/#live-demo
 
-    let isAnnotating = false;
+//var localStream;
+//var inputCtx = $('#rtcRenderer').getContext('2d');
+
+"use strict";
+
+let isAnnotating = false;
+
+window.onload = function () {
+    initialize();
+};
+
+function initialize() {
+    try {
+        document
+            .getElementById("annotate_btn")
+            .addEventListener("click", onAnnotateButtonPressed);
+    } catch (e) {
+        console.log("Initilization error: " + e.message || e, true);
+    }
+}
+
+function onAnnotateButtonPressed() {
+    if (isAnnotating) {
+        stopAnnotating();
+    } else {
+        startAnnotating();
+    }
+}
+
+function stopAnnotating() {
+    let e = document.getElementById("annotate_btn");
+    let v = e.value;
+    e.value = "Start Annotate";
+    isAnnotating = false;
+}
+
+function startAnnotating() {
+    let e = document.getElementById("annotate_btn");
+    let v = e.value;
+    e.value = "Stop Annotate";
+    isAnnotating = true;
+
+    //navigator.getUserMedia({ video: true },
+    //    stream => {
+    //        alert('successfully got stream');
+    //        localStream = stream;
+    //        //$('.local video').attr('src', URL.createObjectURL(stream));
+    //        //drawToCanvas();
+    //    },
+    //    error => {
+    //        alert('error while accessing usermedia ' + error.toString());
+    //    }
+    //);
+
+    //drawToCanvas();
 
 
-    window.onload = function () {
-        initialize();
-    };
+}
 
-    function initialize() {
-        try {
-            document
-                .getElementById("annotate_btn")
-                .addEventListener("click", onAnnotateButtonPressed);
-        } catch (e) {
-            showMessage("Initilization error: " + e.message || e, true);
-        }
+function drawToCanvas() {
+
+    let localVideo = $('#rtcRenderer')[0];
+    //let videoRenderer = document.getElementById("rtcRenderer");
+    let inputCtx = $('.input-canvas canvas')[0].getContext('2d');
+    let width = 300;
+    let height = 225;
+    let outputCanvas = $('.output-canvas canvas')[0];
+    let outputCtx = outputCanvas.getContext('2d');
+
+    // draw video to input canvas
+    inputCtx.drawImage(localVideo, 0, 0, width, height);
+
+    // get pixel data from input canvas
+    var pixelData = inputCtx.getImageData(0, 0, width, height);
+
+    var avg, i;
+
+    // simple greyscale transformation
+    for (i = 0; i < pixelData.data.length; i += 4) {
+        avg = (pixelData.data[i] + pixelData.data[i + 1] + pixelData.data[i + 2]) / 3;
+        pixelData.data[i] = avg;
+        pixelData.data[i + 1] = avg;
+        pixelData.data[i + 2] = avg;
     }
 
-    function onAnnotateButtonPressed() {
-        if (isAnnotating) {
-            stopAnnotating();
-        } else {
-            startAnnotating();
-        }
-    }
+    outputCtx.putImageData(pixelData, 0, 0);
+    requestAnimationFrame(drawToCanvas);
 
-    function stopAnnotating() {
-        let e = document.getElementById("annotate_btn");
-        let v = e.value;
-        e.value = "Start Annotate";
-        isAnnotating = false;
-    }
+    let videoPreview = document.getElementById("previewVideo");
+    videoPreview.srcObject = outputCanvas.captureStream();
 
-    function startAnnotating() {
-        let e = document.getElementById("annotate_btn");
-        let v = e.value;
-        e.value = "Stop Annotate";
-        isAnnotating = true;
-    }
+}
 
 
-})(typeof window === "object" ? window : global);
